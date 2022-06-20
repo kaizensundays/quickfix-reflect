f789ffc2dac90588136cae90e387e2376612efa3
KaizenSundays <kaizensundays@localhost>
2022-06-20 12:25:53 -0400

    [ quickfix-reflect ] SomeTest

---
+++ src/test/java/com/kaizensundays/fusion/quickfix/SomeTest.kt
@@ -0,0 +1,20 @@
+package com.kaizensundays.fusion.quickfix
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import org.junit.Test
+
+/**
+ * Created: Monday 6/20/2022, 12:15 PM Eastern Time
+ *
+ * @author Sergey Chuykov
+ */
+class SomeTest {
+
+    val xml = XmlMapper()
+
+    @Test
+    fun test() {
+
+    }
+
+}
