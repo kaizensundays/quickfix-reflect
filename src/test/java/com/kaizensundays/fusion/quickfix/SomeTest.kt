cd839d05a29d680520c6df8de52620d947a51de6
KaizenSundays <kaizensundays@localhost>
2022-06-25 12:31:18 -0400

    [ quickfix-reflect ] NewOrderSingle

--- src/test/java/com/kaizensundays/fusion/quickfix/SomeTest.kt
+++ src/test/java/com/kaizensundays/fusion/quickfix/SomeTest.kt
@@ -1,6 +1,7 @@
 package com.kaizensundays.fusion.quickfix
 
 import com.kaizensundays.fusion.quickfix.fix44.FixType
+import com.kaizensundays.fusion.quickfix.messages.NewOrderSingle
 import org.junit.Test
 import org.springframework.core.io.ClassPathResource
 import java.io.FileInputStream
@@ -41,6 +42,22 @@ class SomeTest {
         assertEquals("OrderQty", fieldMap["38"]?.name)
         assertEquals("Side", fieldMap["54"]?.name)
         assertEquals("Symbol", fieldMap["55"]?.name)
+
+    }
+
+    fun String.firstCharToUpper() = replaceFirstChar { c -> c.uppercase() }
+
+    @Test
+    fun reflect() {
+
+        val c = NewOrderSingle::class.java
+
+        val declaredFields = c.declaredFields
+        val fields = c.fields
+
+        val fieldMap = fields.map { f -> f.name.firstCharToUpper() to f }.toMap()
+
+        fields.forEach { f -> println(f.name.firstCharToUpper()) }
 
     }
 
